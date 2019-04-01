b31805075fe6096d95262198a845ddda7016af0e
Joan Sánchez <joansvich@gmail.com>
2019-04-01 17:42:03 +0200

    Update checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -4,15 +4,21 @@ const stripe = require("stripe")("sk_test_cVixdDOqymfQhjyDEMVFeKQg00zpmzUPXB");
 const { isLoggedIn, isNotLoggedIn, validationLoggin } = require('../helpers/middlewares');
 
 router.post('/', isLoggedIn, async (req, res, next) => {
-  const {amount, token} = req.body;
-  const charge = await stripe.charges.create({
-    amount,
-    currency: 'eur',
-    description: 'Participación por el coche',
-    source: token
-  });
-  res.json(charge);
-  res.status(200);
+  try {
+    const { amount, token } = req.body;
+    const charge = await stripe.charges.create({
+      amount,
+      currency: 'eur',
+      description: 'Participación por el coche',
+      source: token
+    });
+    res.json(charge);
+    res.status(200);
+  } catch (error) {
+    res.json(error);
+    res.status(400);
+  }
+
 });
 
 
