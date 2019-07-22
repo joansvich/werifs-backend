a9f8cc39f5e65e5e818536d6a4cdb4c7d5effa2e
Joan Sánchez <joansvich@gmail.com>
2019-07-22 17:15:41 +0200

    Update

---
+++ public/precache-manifest.bddb0a35cfa577648dc9363f6557544a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211b057c191d3fa7be80c28e41ea61dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6ec8dba1a464826ea15",
+    "url": "/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "136d67dcafba919fa1cd",
+    "url": "/static/css/main.85105bdc.chunk.css"
+  },
+  {
+    "revision": "b6ec8dba1a464826ea15",
+    "url": "/static/js/2.cad63ad5.chunk.js"
+  },
+  {
+    "revision": "136d67dcafba919fa1cd",
+    "url": "/static/js/main.eec02454.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
