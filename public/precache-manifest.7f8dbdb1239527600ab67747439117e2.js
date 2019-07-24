2f89b60babb1c720c175a51166cc9304957ad95a
Joan Sánchez <joansvich@gmail.com>
2019-07-24 15:18:44 +0200

    Update

--- public/precache-manifest.7f8dbdb1239527600ab67747439117e2.js
+++ public/precache-manifest.7f8dbdb1239527600ab67747439117e2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b420f751f2e445865729e33844ecc411",
+    "revision": "8f1557a190229d6052fdc29656d7188b",
     "url": "/index.html"
   },
   {
-    "revision": "475cb06adf2a67688f9a",
+    "revision": "a068e3285c2ea599333b",
     "url": "/static/css/2.509658c0.chunk.css"
   },
   {
-    "revision": "c4b302b895e5620b2c03",
+    "revision": "d01d9346342923849b4e",
     "url": "/static/css/main.85105bdc.chunk.css"
   },
   {
-    "revision": "475cb06adf2a67688f9a",
-    "url": "/static/js/2.5dd934f0.chunk.js"
+    "revision": "a068e3285c2ea599333b",
+    "url": "/static/js/2.872463fe.chunk.js"
   },
   {
-    "revision": "c4b302b895e5620b2c03",
-    "url": "/static/js/main.f6f73bad.chunk.js"
+    "revision": "d01d9346342923849b4e",
+    "url": "/static/js/main.44948720.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
